c6aa6e3f3bb1883181150cfbc415b54e1fb6d31d
AbdSh17 <abdalraheem200417a@gmail.com>
2025-01-20 11:09:10 +0200

    Edit DAY1

--- Days/Day1/TASK2/task2_solution.py
+++ Days/Day1/TASK2/task2_solution.py
@@ -1,1 +1,13 @@
-my_name
+my_name = "Rasheed"
+my_age = 22
+my_school = "Birziet University"
+my_class = 7
+my_favorite_color = "green"
+my_favorite_food = "mansaf"
+
+print(f"My name is {my_name},")
+print(f"I am {my_age} years old,")
+print(f"I go to {my_school},")
+print(f"I am in {my_class}th grade,")
+print(f"My favorite color is {my_favorite_color},")
+print(f"My favorite food is {my_favorite_food}.")
